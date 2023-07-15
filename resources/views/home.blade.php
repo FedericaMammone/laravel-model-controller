8dd7a3386cb4d781fb49b31a280f3c22ff81961f
Fef97 <federicakr97@gmail.com>
2023-07-15 23:16:20 +0200

    Creata struttura base e database. Stampati a schermo dati db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>hello world from home</h1>
+    <h1>Movie</h1>
+
+    @foreach ($movies as $movie)
+        <ul>
+            <li>
+                {{ $movie->title }}
+            </li>
+        </ul>
+    @endforeach
 @endsection
