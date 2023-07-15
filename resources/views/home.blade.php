92f356797d8e04de18e83debd53f1e18fb76af61
Fef97 <federicakr97@gmail.com>
2023-07-15 22:59:18 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main-layout')
+
+@section('content')
+    <h1>hello world from home</h1>
+@endsection
---
+++ resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+    <title>Document</title>
+</head>
+
+<body>
+    @include('partials.header')
+
+
+    @yield('content')
+    @include('partials.footer')
+</body>
+
+</html>
